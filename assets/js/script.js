4ec08a2d4f8777283d45b414b729876badb1cd91
James Ryan IV <Natecrunchyroll@gmail.com>
2024-06-09 17:42:47 -0400

    Updated index.html, script.js, style.css.  Removed all bulma colors from index.html, used coolors.co to create a template of colors and variables, created a root section in style.css and added template to that.  Added other API parameter to script.js, haven't work on functionality yet. Trimmed index.html to be more inline with the demo and formatted it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
-// Weather API key = 0859c7d7315bb3c27acac4f80f84a44b
+function fetchCoordinates(cityName) {
+    const apiKey = `0859c7d7315bb3c27acac4f80f84a44b`;
+    const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=`;
+    // {city name},{state code},{country code}&limit={limit}&appid={API key}
+    // ex:https://api.openweathermap.org/geo/1.0/direct?q=dallas,texas,us&limit=1&appid=0859c7d7315bb3c27acac4f80f84a44b
+    const latLonUrl = `https://api.openweathermap.org/data/2.5/forecast?`;
+    // lat={lat}&lon={lon}&appid={API key}
+   
+}
 
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0859c7d7315bb3c27acac4f80f84a44b}
