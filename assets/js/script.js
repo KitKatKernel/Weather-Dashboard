60e446841da3dc823a74ed8cb922cfa853895096
James Ryan IV <Natecrunchyroll@gmail.com>
2024-06-09 19:34:50 -0400

    Updated index.html, styles.css, script.js.  Started working on API functionality in script.js.  Updated index.html to include state and country optionals, may remove if not needed later.  Did test API and my town returned the top result in england, so that's my solution for now.  Stylized some more of the page in style.css to look like dusk, morning, afternoon, sunset, night colors.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-function fetchCoordinates(cityName) {
-    const apiKey = `0859c7d7315bb3c27acac4f80f84a44b`;
-    const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=`;
-    // {city name},{state code},{country code}&limit={limit}&appid={API key}
-    // ex:https://api.openweathermap.org/geo/1.0/direct?q=dallas,texas,us&limit=1&appid=0859c7d7315bb3c27acac4f80f84a44b
-    const latLonUrl = `https://api.openweathermap.org/data/2.5/forecast?`;
-    // lat={lat}&lon={lon}&appid={API key}
-   
-}
+const apiKey = `0859c7d7315bb3c27acac4f80f84a44b`;
+const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=`;
+// {city name},{state code},{country code}&limit={limit}&appid={API key}
+// ex:https://api.openweathermap.org/geo/1.0/direct?q=dallas,texas,us&limit=1&appid=0859c7d7315bb3c27acac4f80f84a44b
+const latLonUrl = `https://api.openweathermap.org/data/2.5/forecast?`;
+// lat={lat}&lon={lon}&appid={API key}
+const citySearch = document.querySelector('.city-input');
+const cityButton = document.querySelector('.city-button');
 
+   cityButton.addEventListener('click', function() {
+    let city = citySearch.value;
+
+   });
