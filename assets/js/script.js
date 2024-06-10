4bc2f87b6c7079a2ab41e147e0c25e6ced294277
James Ryan IV <Natecrunchyroll@gmail.com>
2024-06-09 20:45:22 -0400

    Updated index.html, script.js.  Linked script in index.html, added citySearchButton function, which takes in city, state, country input values and records the .lat and .lon and prints them to console.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,45 @@
 const apiKey = `0859c7d7315bb3c27acac4f80f84a44b`;
 const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=`;
-// {city name},{state code},{country code}&limit={limit}&appid={API key}
-// ex:https://api.openweathermap.org/geo/1.0/direct?q=dallas,texas,us&limit=1&appid=0859c7d7315bb3c27acac4f80f84a44b
-const latLonUrl = `https://api.openweathermap.org/data/2.5/forecast?`;
-// lat={lat}&lon={lon}&appid={API key}
-const citySearch = document.querySelector('.city-input');
-const cityButton = document.querySelector('.city-button');
+const citySearchButton = document.querySelector('.city-button');
+const citySearchInput = document.querySelector('.city-input');
+const stateSearch = document.querySelector('.state-input');
+const countrySearch = document.querySelector('.country-input');
 
-   cityButton.addEventListener('click', function() {
-    let city = citySearch.value;
+
+   citySearchButton.addEventListener('click', function() {
+    console.log('button clicked')
+    let city = citySearchInput.value;
+    let state = stateSearch.value;
+    let country = countrySearch.value;
+    console.log ('City:', city);
+    console.log ('State:', state);
+    console.log ('Country:', country);
+    
+    const limit = 1;
+    let query = city;
+    if (state) {
+        query += `,${state}`;
+    }
+    if (country) {
+        query += `,${country}`;
+    }
+    const cityApiUrl = `${cityUrl}${query}&limit=${limit}&appid=${apiKey}`;
+
+    fetch(cityApiUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        if (data.length > 0) {
+            const lat = data[0].lat
+            const lon = data[0].lon
+            console.log('lat:', lat, 'lon:', lon)
+        } else {
+            console.log ('City not found')
+        }
+    })
+    .catch(function(error) {
+        console.error('Error fetching the coordinates:', error);
+    })
 
    });
